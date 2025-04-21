1954bd8560f881c8add7a7cbd4c11a1b89baff84
sibmaks <sibmaks@vk.com>
2025-04-21 17:56:52 +0300

    Initial commit

---
+++ src/main/java/com/github/sibmaks/springdocsrender/SpringDocsRenderApplication.java
@@ -0,0 +1,13 @@
+package com.github.sibmaks.springdocsrender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDocsRenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDocsRenderApplication.class, args);
+    }
+
+}
