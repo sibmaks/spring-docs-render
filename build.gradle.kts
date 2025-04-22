dfeb0ca5b06e3281b5b8174ff7a4e444c21374e8
sibmaks <sibmaks@vk.com>
2025-04-22 17:25:59 +0300

    Fix bar code generation
    (cherry picked from commit 5115c22a3d8eaf4b5da755a7e036e881718f29a4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     implementation("net.sf.jasperreports:jasperreports:6.21.3")
+    implementation("net.sf.barcode4j:barcode4j:2.1")
+    implementation("com.google.zxing:core:3.5.3")
+    implementation("org.apache.xmlgraphics:batik-bridge:1.18")
     implementation("io.github.sibmaks.spring-jfr:agent:0.0.29")
 
     compileOnly("org.projectlombok:lombok")
